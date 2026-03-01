cf10bf067a8ebc5114678caf224b3ff101659b7d
C191239 <zsz2007314zsz@sina.com>
2026-02-28 23:59:40 -0600

    finish hw2

---
+++ hw2/H2Q4.java
@@ -0,0 +1,26 @@
+import java.util.Collections;
+import java.util.stream.IntStream;
+
+/**
+ * @author Shunzhong Zhu
+ * @version 0.1.0
+ * @since 2026-02-28
+ */
+public class H2Q4 {
+    public static void main(String[] args) {
+        int[] arr = IntStream.generate(() -> roll() + roll()).limit(1000).toArray();
+        int[] freq = new int[11];
+        for (int x : arr) freq[x - 2]++;
+        for (int i = 0; i < 11; i++)
+            System.out.println("Sum " + (i + 2) + (i + 2 < 10 ? " " : "") + ": " + String.join("", Collections.nCopies(freq[i], "*")) + " " + freq[i]);
+    }
+
+    /**
+     * Roll dice for once.
+     *
+     * @return number rolled
+     */
+    private static int roll() {
+        return (int) Math.ceil(Math.random() * 6);
+    }
+}
