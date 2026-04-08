212db70e9765f9337f243478e5a087caa72ac568
C191239 <zsz2007314zsz@sina.com>
2026-04-08 11:00:21 -0500

    optimize

--- inclass/Fibonacci.java
+++ inclass/Fibonacci.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Fibonacci {
     private static int fib(int x) {
         if (x <= 0) return 0;
-        if (x == 1 || x == 2) return 1;
+        if (x == 1) return 1;
         return fib(x - 1) + fib(x - 2);
     }
 
