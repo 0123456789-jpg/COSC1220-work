c4e44d655774f7b9ec3e0e38bca37d2ccb4313f9
C191239 <zsz2007314zsz@sina.com>
2026-04-24 10:55:14 -0500

    in-class

---
+++ inclass/Time.java
@@ -0,0 +1,45 @@
+/**
+ * @author Shunzhong Zhu
+ * @version 0.1.0
+ * @since 2026-04-22
+ */
+public class Time {
+    private int hour;
+    private int minute;
+    private int second;
+
+    public static void main(String[] args) {
+        Time time1 = new Time();
+        displayTime("After time object is created", time1);
+        System.out.println();
+        time1.setTime(13, 27, 6);
+        displayTime("After calling setTime", time1);
+        System.out.println();
+        try {
+            time1.setTime(99, 99, 99);
+        } catch (IllegalArgumentException e) {
+            System.out.printf("Exception: %s%n%n", e.getMessage());
+        }
+        displayTime("After calling setTime with invalid values", time1);
+    }
+
+    private static void displayTime(String header, Time t) {
+        System.out.printf("%s%nUniversal Time: %s%nStandard time: %s%n", header, t.toUniversalString(), t);
+    }
+
+    private void setTime(int hour, int minute, int second) {
+        if (hour >= 24 || minute >= 60 || second >= 60)
+            throw new IllegalArgumentException("Illegal time representation");
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public String toUniversalString() {
+        return String.format("%02d:%02d:%02d", hour, minute, second);
+    }
+
+    public String toString() {
+        return String.format("%d:%02d:%02d %s", ((hour == 0 || hour == 12) ? 12 : hour % 12), minute, second, (hour < 12 ? "AM" : "PM"));
+    }
+}
